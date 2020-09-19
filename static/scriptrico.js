abd1e3cf9380ec5437bd034e8b9498969be4a572
dylan-robins <dylan.robins@gmail.com>
2020-09-19 23:43:54 +0200

    Ajout CSS + structure HTML basique

--- static/scriptrico.js
+++ static/scriptrico.js
@@ -1,14 +1,18 @@
 function send_Gcode_to_motor(sens) {
     if ((sens == "gauche") || (sens == "init") || (sens == "parametres") || (sens == "droite") || (sens == "haut") || (sens == "bas")) {
+        // Zone de texte où écrire le log des actions
+        const zone_text = document.getElementById("msg")
 
+        // Créer une requête
         let req = new XMLHttpRequest();
+        // Définir l'action à prendre une fois la réponse du serveur reçue
         req.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-                console.log("ok send")
-                let zone_text = document.getElementById("msg")
-                zone_text.innerText = this.responsesend_Gcode_to_motorText
+                //  Si succès, écrire le status + le message reçu sur la page
+                zone_text.innerText += this.status + " - " + this.responsesend_Gcode_to_motorText + "\n"
             } else if (this.readyState == 4) {
-                console.log("Unable to fetch data... Error ", this.status);
+                //  Si erreur, écrire l'erreur sur la page
+                zone_text.innerText += this.status + " - Error" + "\n"
             }
         }
         req.open('POST', '/api', true);
